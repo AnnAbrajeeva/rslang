3a128f5d062b56c1ff4df2f7c5863eff5c08eaa5
Shahzod Ubaydullaev <shahzodubaydullaev2206@gmail.com>
2022-08-16 13:34:54 +0500

    fix: fix conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useState } from "react";
 import Navbar from "./components/Main page/Navbar";
@@ -58,31 +57,7 @@ function App() {
         </div>
       </ThemeProvider>
     </Router>
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> fb29068c904c61c3b33591eeae751bbd4f070c25
-  );
+  )
 }
 
 export default App;
