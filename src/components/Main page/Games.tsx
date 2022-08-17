46c7dade5cdb7a45db43ed5658229afb4a48c755
Shahzod Ubaydullaev <shahzodubaydullaev2206@gmail.com>
2022-08-17 11:15:07 +0500

    feat: add games page

--- src/components/Main page/Games.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-const Games = (props: { setIsFooter: (arg0: boolean) => void }) => {
-  useEffect(() => props.setIsFooter(false), [props]);
-
-  return <p>Games</p>;
-};
-
-export default Games;
