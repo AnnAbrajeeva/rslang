dccb52f3b0990bc8b33138e8b65b0d4741e62389
Shahzod Ubaydullaev <shahzodubaydullaev2206@gmail.com>
2022-08-29 11:23:30 +0500

    feat: add deploy

--- src/components/Games page/Sprint/SprintGameCont.tsx
+++ src/components/Games page/Sprint/SprintGameCont.tsx
@@ -37,7 +37,7 @@ const SprintContainer = (props: {
 
   useEffect(() => {
     let randomPage = Math.floor(Math.random() * 30)
-    api.getAllWords(10, props.level - 1).then((data) => setWords(data))
+    api.getAllWords(randomPage, props.level - 1).then((data) => setWords(data))
   }, [])
 
   useEffect(() => {
