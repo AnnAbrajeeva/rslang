fb7ebc9cc60dbe36710b45af1f584123427e4c75
Shahzod Ubaydullaev <shahzodubaydullaev2206@gmail.com>
2022-09-05 21:35:42 +0500

    feat: finish project

--- src/components/Registration/components/RegistrationFrom/utils/validationSchema.ts
+++ src/components/Registration/components/RegistrationFrom/utils/validationSchema.ts
@@ -1,9 +1,9 @@
-import * as yup from 'yup';
+import * as yup from 'yup'
 
 const validationSchema = yup.object({
   username: yup
     .string()
-    .min(6, 'Username must contain at least 6 characters')
+    .min(2, 'Username must contain at least 2 characters')
     .required('Requires the user name to be entered'),
   email: yup
     .string()
@@ -13,13 +13,10 @@ const validationSchema = yup.object({
     .string()
     .min(8, 'Password must contain at least 8 characters')
     .required('A password is required'),
-  changepassword: yup.string().when("password", {
-    is: (val: string) => (!!(val && val.length > 0)),
-    then: yup.string().oneOf(
-      [yup.ref("password")],
-      "Passwords must match"
-    )
-  })
-});
+  changepassword: yup.string().when('password', {
+    is: (val: string) => !!(val && val.length > 0),
+    then: yup.string().oneOf([yup.ref('password')], 'Passwords must match'),
+  }),
+})
 
-export default validationSchema;
+export default validationSchema
